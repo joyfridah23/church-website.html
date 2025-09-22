0413a5ab7e26b7db66298f50f34379632539caed
joyfridah23 <ndambukijoyfridah@gmail.com>
2025-09-22 17:00:26 +0300

    Update Contact component to correct phone number formatting and enhance messaging clarity

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,11 +10,16 @@ function Contact() {
             </p>
             <div className="bg-white rounded-lg shadow p-6 w-full max-w-md text-center">
                 <p className="text-xl font-semibold mb-2">Phone:</p>
-                <p className="text-blue-900 text-lg mb-4">07 xxxxxxxx</p>
-                <p className="text-gray-700">You can call or text us for more information about our services, events.
-                    To connect with  pastor, call 07XXXXXXXX.</p>
+                <p className="text-100-900 text-lg mb-4">07XXXXXXXX</p>
+                <p className="text-100-900 text-lg mb-4">07XXXXXXXX</p>
+                <p className="text-gray-700" />
             </div>
+            <div className='bg-blue-800 rounded-lg shadow p-6 w-full max-w-md text-center mt-4'>
+                <p className='text-white text-lg'> To connect with  pastor, call 07XXXXXXXX.</p>
+            </div>
+
         </div>
+
     );
 }
 
